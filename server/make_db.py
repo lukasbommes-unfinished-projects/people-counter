4f7f5593b049a6bd448cc4dbb6c101c50c765a23
LukasBommes <mail@lukasbommes.de>
2019-08-14 11:39:37 +0800

    added user registration and login

--- server/make_db.py
+++ server/make_db.py
@@ -1,37 +1,42 @@
 import datetime
+import random
 from server import db
 from server import User, Room, Camera, Count
 
-db.create_all()
+# db.create_all()
+#
+# admin = User(username='admin', email='admin@example.com')
+# guest = User(username='guest', email='guest@example.com')
+#
+# db.session.add(admin)
+# db.session.add(guest)
+# db.session.commit()
+#
+# room0 = Room(name="Dining Hall", description="Dining hall on L3")
+# room1 = Room(name="Hallway North", description="")
+# room2 = Room(name="Hallway South", description="Temporary arrangement")
+# room3 = Room(name="Entrance", description="")
+#
+# cam0 = Camera(url="rtsp:://123.sdp", username="user1", password="pw1")
+# cam1 = Camera(url="rtsp:://456.sdp", username="user1", password="pw1")
+#
+# room0.cameras.append(cam0)
+# room0.cameras.append(cam1)
+#
+# db.session.add(room0)
+# db.session.add(room1)
+# db.session.add(room2)
+# db.session.add(room3)
+# db.session.commit()
+#
+# print(room0.cameras)
+# print(cam0.query.with_parent(room0).filter(Camera.id != 2).all())
 
-admin = User(username='admin', email='admin@example.com')
-guest = User(username='guest', email='guest@example.com')
-
-db.session.add(admin)
-db.session.add(guest)
-db.session.commit()
-
-room0 = Room(name="Dining Hall", description="Dining hall on L3")
-room1 = Room(name="Hallway North", description="")
-room2 = Room(name="Hallway South", description="Temporary arrangement")
-room3 = Room(name="Entrance", description="")
-
-cam0 = Camera(url="rtsp:://123.sdp", username="user1", password="pw1")
-cam1 = Camera(url="rtsp:://456.sdp", username="user1", password="pw1")
-
-room0.cameras.append(cam0)
-room0.cameras.append(cam1)
-
-db.session.add(room0)
-db.session.add(room1)
-db.session.add(room2)
-db.session.add(room3)
-db.session.commit()
-
-print(room0.cameras)
-print(cam0.query.with_parent(room0).filter(Camera.id != 2).all())
-
-count0 = Count(timestamp=datetime.datetime.now(), people_count=35)
-cam0.counts.append(count0)
+for i in range(100):
+    count = Count(timestamp=datetime.datetime.now(), people_count=random.random())
+    cams = Camera.query.all()
+    print(cams)
+    i = random.choice(range(len(cams)))
+    cams[i].counts.append(count)
 
 db.session.commit()
--- server/server.py
+++ server/server.py
@@ -4,29 +4,47 @@ import json
 
 from flask import Flask, render_template, jsonify, Response, make_response, abort, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
-from flask_httpauth import HTTPBasicAuth
+#from flask_httpauth import HTTPBasicAuth
+
+from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.urls import url_parse
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, TextAreaField, SelectField, SubmitField
-from wtforms.validators import InputRequired, Optional, Length
+from wtforms import StringField, PasswordField, TextAreaField, SelectField, SubmitField, BooleanField
+from wtforms.validators import InputRequired, Optional, Length, ValidationError, Email, EqualTo
 
 from config import Config
 
+# Initialization
 app = Flask(__name__)
 app.config.from_object(Config)
+
+login_manager = LoginManager()  # for user login
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
 db = SQLAlchemy(app)
-auth = HTTPBasicAuth()
+
+#auth = HTTPBasicAuth()  # for securing REST API
 
 
 # Database ORM
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User {}>'.format(self.username)
 
 
 class Room(db.Model):
@@ -36,7 +54,7 @@ class Room(db.Model):
     cameras = db.relationship("Camera", cascade="save-update, delete", backref="room", lazy=True)
 
     def __repr__(self):
-        return '<Room %r>' % self.name
+        return '<Room {}>'.format(self.name)
 
     def to_dict(self):
        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
@@ -50,11 +68,11 @@ class Camera(db.Model):
     room_id = db.Column(db.Integer, db.ForeignKey('room.id'), nullable=False)
     counts = db.relationship("Count", cascade="save-update, delete", backref="camera", lazy=True)
 
-    def __repr__(self):
-        return '<Camera %r>' % self.url
-
     def to_dict(self):
        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
+    def __repr__(self):
+        return '<Camera {}>'.format(self.url)
 
 
 class Count(db.Model):
@@ -63,14 +81,39 @@ class Count(db.Model):
    people_count = db.Column(db.Integer, nullable=False)
    cam_id = db.Column(db.Integer, db.ForeignKey('camera.id'), nullable=False)
 
-   def __repr__(self):
-      return '<Count %r>' % self.id
-
    def to_dict(self):
       return {c.name: getattr(self, c.name) for c in self.__table__.columns}
 
+   def __repr__(self):
+      return '<Count {}>'.format(self.id)
+
 
 # Forms
+
+class LoginForm(FlaskForm):
+    username = StringField("Username", validators=[InputRequired(), Length(max=64)])
+    password = PasswordField("Password", validators=[InputRequired(), Length(max=128)])
+    submit = SubmitField("Login")
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired(), Length(max=64)])
+    email = StringField('Email', validators=[InputRequired(), Email(), Length(max=120)])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=12, max=128)])
+    password2 = PasswordField(
+        'Repeat Password', validators=[InputRequired(), EqualTo('password'), Length(min=12, max=128)])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('The username is already registered.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('The email address is already registered.')
+
 
 class RoomForm(FlaskForm):
     name = StringField("Name", validators=[InputRequired(), Length(max=64)])
@@ -86,6 +129,55 @@ class CameraForm(FlaskForm):
     submit = SubmitField("Save changes")
 
 
+# User Login & Registration
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(username=login_form.username.data).first()
+        if user is None or not user.check_password(login_form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        flash('Sucessfully logged in')
+        return redirect(next_page)
+    return render_template('login.html', login_form=login_form)
+
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    flash('Sucessfully logged out')
+    return redirect(url_for('index'))
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    register_form = RegistrationForm()
+    if register_form.validate_on_submit():
+        user = User(username=register_form.username.data, email=register_form.email.data)
+        user.set_password(register_form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Sucessfully registered')
+        return redirect(url_for('login'))
+    return render_template('register.html', register_form=register_form)
+
+
 # UI
 
 @app.route("/")
@@ -93,7 +185,14 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/loggedinonly")
+@login_required
+def loggedinonly():
+    return "You see this only because you are logged in!"
+
+
 @app.route("/setup-cameras", methods=["GET", "POST"])
+@login_required
 def setup_cameras():
     camera_form = CameraForm()
     rooms = Room.query.all()
@@ -112,9 +211,10 @@ def setup_cameras():
 
 
 @app.route("/setup-cameras/edit-camera/<int:cam_id>", methods=["GET", "POST"])
+@login_required
 def edit_camera(cam_id):
     cam = Camera.query.get(cam_id)
-    camera_data = {"url": cam.url, "username": cam.username, "password": cam.password}
+    camera_data = {"url": cam.url, "username": cam.username, "password": cam.password, "rooms": cam.room_id}
     camera_form = CameraForm(data=camera_data)
     rooms = Room.query.all()
     camera_form.rooms.choices = [(room.id, room.name) for room in rooms]
@@ -133,6 +233,7 @@ def edit_camera(cam_id):
 
 
 @app.route("/setup-cameras/remove-camera/<int:cam_id>", methods=["GET", "POST"])
+@login_required
 def remove_camera(cam_id):
     cam = Camera.query.get(cam_id)
     if request.method == "POST":
@@ -146,6 +247,7 @@ def remove_camera(cam_id):
 
 
 @app.route("/setup-rooms", methods=["GET", "POST"])
+@login_required
 def setup_rooms():
     room_form = RoomForm()
     if room_form.validate_on_submit():
@@ -159,6 +261,7 @@ def setup_rooms():
 
 
 @app.route("/setup-rooms/edit-room/<int:room_id>", methods=["GET", "POST"])
+@login_required
 def edit_room(room_id):
     room = Room.query.get(room_id)
     room_data = {"name": room.name, "description": room.description}
@@ -174,6 +277,7 @@ def edit_room(room_id):
 
 
 @app.route("/setup-rooms/remove-room/<int:room_id>", methods=["GET", "POST"])
+@login_required
 def remove_room(room_id):
     room = Room.query.get(room_id)
     if request.method == "POST":
@@ -186,6 +290,7 @@ def remove_room(room_id):
 
 
 @app.route("/setup-general")
+@login_required
 def setup_general():
     return render_template("setup-general.html")
 
@@ -197,26 +302,26 @@ def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 
-@app.route("/people-counter/api/v1.0/rooms", methods=["GET"])
-#@auth.login_required
-def get_rooms():
-    rooms = Room.query.all()
-    if not rooms:
-        abort(404)
-    return Response(json.dumps([room.to_dict() for room in rooms]),  mimetype='application/json')
-
-
-@app.route("/people-counter/api/v1.0/rooms/<int:room_id>", methods=["GET"])
-#@auth.login_required
-def get_room(room_id):
-    room = Room.query.get(room_id)
-    if not room:
-        abort(404)
-    return Response(json.dumps(room.to_dict()),  mimetype='application/json')
+# @app.route("/people-counter/api/v1.0/rooms", methods=["GET"])
+# #@auth.login_required
+# def get_rooms():
+#     rooms = Room.query.all()
+#     if not rooms:
+#         abort(404)
+#     return Response(json.dumps([room.to_dict() for room in rooms]),  mimetype='application/json')
+#
+#
+# @app.route("/people-counter/api/v1.0/rooms/<int:room_id>", methods=["GET"])
+# #@auth.login_required
+# def get_room(room_id):
+#     room = Room.query.get(room_id)
+#     if not room:
+#         abort(404)
+#     return Response(json.dumps(room.to_dict()),  mimetype='application/json')
 
 
 @app.route("/people-counter/api/v1.0/counts", methods=["GET"])
-@auth.login_required
+#@auth.login_required
 def get_counts():
     counts = []
     room_ids = [room[0] for room in db.get_rooms()]
@@ -227,7 +332,7 @@ def get_counts():
 
 
 @app.route("/people-counter/api/v1.0/counts/<int:room_id>", methods=["GET"])
-@auth.login_required
+#@auth.login_required
 def get_count(room_id):
     count = _get_count(db, room_id)
     return jsonify({"room_id": room_id, "people-count": count})
